f2f351fd5faa7efcada3e5e97a4625b590e14bf2
Korotkevich_Natalia <nataliyakorotkevich15@gmail.com>
2025-01-26 17:20:26 +0300

    Add code to use add_numbers function

--- labgitproject-Korotkevich/src/main.py
+++ labgitproject-Korotkevich/src/main.py
@@ -1,3 +1,18 @@
 # main.py
 if __name__ == "__main__":
     print("Hello, world!")
+
+from src.utils import add_numbers, multiply_numbers
+
+def main():
+    # Использование функции add_numbers
+    a, b = 5, 3
+    sum_result = add_numbers(a, b)
+    print(f"The sum of {a} and {b} is: {sum_result}")
+
+    # Использование функции multiply_numbers
+    product_result = multiply_numbers(a, b)
+    print(f"The product of {a} and {b} is: {product_result}")
+
+if __name__ == "__main__":
+    main()
