34d65069981375fc5cafb7144929eaf35bfb0701
Korotkevich_Natalia <nataliyakorotkevich15@gmail.com>
2025-01-26 17:13:19 +0300

    Add add_numbers and multiply_numbers functions with tests

--- labgitproject-Korotkevich/src/utils.py
+++ labgitproject-Korotkevich/src/utils.py
@@ -1,3 +1,21 @@
 # utils.py
 def example_function():
     pass
+
+def add_numbers(a, b):
+    """
+    Функция для сложения двух чисел.
+    :param a: Первое число.
+    :param b: Второе число.
+    :return: Сумма чисел a и b.
+    """
+    return a + b
+
+def multiply_numbers(a, b):
+    """
+    Функция для умножения двух чисел.
+    :param a: Первое число.
+    :param b: Второе число.
+    :return: Результат умножения a на b.
+    """
+    return a * b
